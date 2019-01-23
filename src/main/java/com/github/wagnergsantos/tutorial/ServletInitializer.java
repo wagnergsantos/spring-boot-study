7c3733ab56beb7ae2e2b438d9b68d1bbecc24246
Wagner Santos <wagner.gsantos@gmail.com>
2019-01-23 09:15:43 -0200

    inicio do projeto

---
+++ src/main/java/com/github/wagnergsantos/tutorial/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.github.wagnergsantos.tutorial;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TutorialApplication.class);
+	}
+
+}
+
