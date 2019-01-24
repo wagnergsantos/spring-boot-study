02a74b5a6ad1064606c7ab235280f5f0c8a9e3b6
Wagner Santos <wagner.gsantos@gmail.com>
2019-01-24 11:22:06 -0200

    correção para evitar erro nos testes

--- src/main/java/com/github/wagnergsantos/tutorial/TutorialApplication.java
+++ src/main/java/com/github/wagnergsantos/tutorial/TutorialApplication.java
@@ -2,8 +2,10 @@ package com.github.wagnergsantos.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class TutorialApplication {
 
 	public static void main(String[] args) {
